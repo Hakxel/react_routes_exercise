2dd0a7b723301c5f086ead67ee213124b2d3ec17
Hakxel <asiel_montes@yahoo.com>
2018-12-20 14:07:31 -0500

    Fix rendering problem

--- React_Routes/src/index.js
+++ React_Routes/src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './CSS/index.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import Happy from './components/Happy';
 import Sleepy from './components/Sleepy';
@@ -10,7 +11,29 @@ import Guilty from './components/Guilty';
 class Home extends Component{
   render(){
     return(
-      // your code goes here
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/happy">Happy</Link>
+            </li>
+            <li>
+              <Link to="/sleepy">Sleepy</Link>
+            </li>
+            <li>
+              <Link to="/guilty">Guilty</Link>
+            </li>
+          </ul>
+          <div>
+            <Route path="/happy" component={Happy} />
+            <Route path="/sleepy" component={Sleepy} />
+            <Route path="/guilty" component={Guilty} />
+          </div>
+        </div>
+      </Router>
     )
   }
 }
